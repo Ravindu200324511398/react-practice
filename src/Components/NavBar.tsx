4db98013dcb7e0ee41e971160009c3972b4edd6f
Ravindu200324511398 <ravindumcg2003@gmail.com>
2026-08-29 10:53:29 +0530

    update nav bar

--- src/Components/NavBar.tsx
+++ src/Components/NavBar.tsx
@@ -7,11 +7,13 @@ function NavBar() {
     <>
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#home">Book</Nav.Link>
+            <Nav.Link href="#features">Staff</Nav.Link>
+            <Nav.Link href="#pricing">Members</Nav.Link>
+            <Nav.Link href="#pricing">Lendings</Nav.Link>
+
           </Nav>
         </Container>
       </Navbar>

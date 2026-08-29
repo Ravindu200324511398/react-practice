e8b3cb6fb82014df769addae7654c23b8a1678f0
Ravindu200324511398 <ravindumcg2003@gmail.com>
2026-08-29 12:52:28 +0530

    Add getBooks function to fetch books from API

---
+++ src/Service/Books/GetBooks.ts
@@ -0,0 +1,13 @@
+import axios from "axios";
+const BASE_URL = "http://localhost:8080/api/v1/books";
+
+export const getBooks = async () => {
+    try{
+        const response = await axios.get(BASE_URL);
+        console.log("Books fetched successfully:", response.data);
+        // return response.data;
+    } catch (error) {
+        console.error("Error fetching books:", error);
+        throw error;
+    }
+}

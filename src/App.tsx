95b280e8df91f4779d44605d4c81135931357474
Ravindu200324511398 <ravindumcg2003@gmail.com>
2026-08-29 11:24:43 +0530

    Add table template

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import NavBar from './Components/Navbar';
+import {BookConsole} from './Components/BookConsole';
 
 function App() {
   return (
     <>
       <NavBar />
+      <BookConsole />
     </>
   )
 }
